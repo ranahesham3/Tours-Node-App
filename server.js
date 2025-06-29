4da2ee485b86956776b282f66f09054a7d8944fa
Rana Hesham <rhesham162@gmail.com>
2025-06-29 22:55:37 +0300

    prepared app for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
 
 process.on('uncaughtException', (err) => {
     console.log(err.name, err.message);
